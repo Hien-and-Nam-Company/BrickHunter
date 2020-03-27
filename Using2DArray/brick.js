566730e7c6e7c09e3165b925be7fa7f2fd7b800a
Yam <maxiaolion@gmail.com>
2020-03-27 14:31:00 +0700

    Add this.row & this.col

--- Using2DArray/brick.js
+++ Using2DArray/brick.js
@@ -1,7 +1,9 @@
 class Brick {
-    constructor(x, y, color) {
+    constructor(x, y, row, col, color) {
         this.x = x;
         this.y = y;
+        this.row = row;
+        this.col = col;
         this.color = color;
     }
 }
