6325926bba4b5a061226b83727d00ef1e5a3772c
Nam <62420404+HoangNamDo@users.noreply.github.com>
2020-03-27 00:52:09 -0700

    Add files via upload

--- Using2DArray/brick.js
+++ Using2DArray/brick.js
@@ -1,9 +1,27 @@
 class Brick {
-    constructor(x, y, row, col, color) {
+    constructor(x, y, col, row, color) {
         this.x = x;
         this.y = y;
-        this.row = row;
         this.col = col;
+        this.row = row;
         this.color = color;
+    }
+
+    get colName() {
+        if (this.col == 0) return 'A';
+        if (this.col == 1) return 'B';
+        if (this.col == 2) return 'C';
+        if (this.col == 3) return 'D';
+        if (this.col == 4) return 'E';
+        if (this.col == 5) return 'F';
+        if (this.col == 6) return 'G';
+    }
+    
+    get rowName() {
+        return ++ this.col;
+    }
+
+    get brickName(){
+        return this.colName + this.row;
     }
 }
--- Using2DArray/game.js
+++ Using2DArray/game.js
@@ -9,6 +9,7 @@ function randomColor() {
 var cols = 7;
 var rows = 15;
 var brickSide = 30;
+var wallOffset = 50;
 var wall = [];
 
 function setupAndDrawWall() {
@@ -19,7 +20,7 @@ function setupAndDrawWall() {
             var x = i * 30;
             var y = j * 30 + 50;
             var color = randomColor();
-            wall[i][j] = new Brick(x, y, color);
+            wall[i][j] = new Brick(x, y, i, j, color);
     // Draw wall
             context.beginPath();
             context.rect(wall[i][j].x, wall[i][j].y, brickSide, brickSide);
@@ -32,3 +33,4 @@ function setupAndDrawWall() {
 }
 
 setupAndDrawWall();
+console.log(wall[2][4].brickName);
