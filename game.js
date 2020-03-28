7b1654dab053f45b549a3c107c80b1673c0fe766
Yam <maxiaolion@gmail.com>
2020-03-28 20:09:25 +0700

    Update game.js

--- game.js
+++ game.js
@@ -15,8 +15,8 @@ bullet = bullet1;
 document.addEventListener("keyup", function (event) {
     if (event.keyCode == 37) { // arrow left: 37;   space keyCode: 32
         bullet.getReady();
-        weapon.color = randomColor();
         bullet.setCanDestroy(true);
+        weapon.color = randomColor();
         // bullet = bullet2;
     }
 })
@@ -26,12 +26,55 @@ function collisionBulletBrick() {
         for (var j = 0; j < rows; j++) {
             if ((wall[i][j].isTouchedBy(bullet)) && (wall[i][j].color == bullet.color)) {
                 wall[i][j].setBroken(true);
-                bullet.setCanDestroy(false);
-            } else if(wall[i][j].isTouchedBy(bullet)){
+                checkAround(i, j, bullet.color);
+            } else if (wall[i][j].isTouchedBy(bullet)) {
                 bullet.setCanDestroy(false);
             }
         }
     }
+}
+
+function checkUp(col, row, color) {
+    if (row > 1) {
+        if (!wall[col][row - 1].isBroken && wall[col][row - 1].color == color) {
+            wall[col][row - 1].setBroken(true);
+            checkAround(col, row - 1, color);
+        }
+    }
+}
+
+function checkDown(col, row, color) {
+    if (row < rows - 1) {
+        if (!wall[col][row + 1].isBroken && wall[col][row + 1].color == color) {
+            wall[col][row + 1].setBroken(true);
+            checkAround(col, row + 1, color);
+        }
+    }
+}
+
+function checkLeft(col, row, color) {
+    if (col > 1) {
+        if (!wall[col - 1][row].isBroken && wall[col - 1][row].color == color) {
+            wall[col - 1][row].setBroken(true);
+            checkAround(col - 1, row, color);
+        }
+    }
+}
+
+function checkRight(col, row, color) {
+    if (col < cols - 1) {
+        if (!wall[col + 1][row].isBroken && wall[col + 1][row].color == color) {
+            wall[col + 1][row].setBroken(true);
+            checkAround(col + 1, row, color);
+        }
+    }
+}
+
+function checkAround(col, row, color) {
+    checkUp(col, row, color);
+    checkDown(col, row, color);
+    checkLeft(col, row, color);
+    checkRight(col, row, color);
 }
 
 function draw() {
