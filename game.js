fd6dff17de58b2745a37abf8b613bcfe0befec0a
Yam <maxiaolion@gmail.com>
2020-03-30 14:14:40 +0700

    Update game.js

--- game.js
+++ game.js
@@ -15,24 +15,30 @@ document.addEventListener("keyup", function (event) {
 function collisionBulletBrick() {
     for (var i = 0; i < cols; i++) {
         for (var j = 0; j < rows; j++) {
-            if (Physics.collision(wall[i][j], bullet)) {
+            if (Physics.collision(wall[i][j], bullet) && !wall[i][j].isBroken) {
                 if (wall[i][j].color == bullet.color) {
                     wall[i][j].setBroken(true);
                     checkAround(i, j, bullet.color);
-                } //else colorEffect(wall[i][j], bullet.color);
-                bullet.stopMovingLeft();
+                } else if (i < cols - 1 && wall[i][j].y == bullet.y) {
+                    wall[i + 1][j].setBroken(false);
+                    wall[i + 1][j].setColor(bullet.color);
+                    drawWall();
+                }
+                bullet.disappear();
+                //else colorEffect(wall[i][j], bullet.color);
+                //bullet.stopMovingLeft();
             }
         }
     }
 }
 
-function draw(){
+function draw() {
     weapon.draw();
     drawWall();
     bullet.draw();
 }
 
-function update(){
+function update() {
     bullet.update();
     collisionBulletBrick();
     checkDropDown();
