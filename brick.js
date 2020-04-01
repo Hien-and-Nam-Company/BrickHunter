790ba773639010039ac283e8511fe9a42cd76fc2
Nam <62420404+HoangNamDo@users.noreply.github.com>
2020-03-31 22:30:30 -0700

    Add files via upload

--- brick.js
+++ brick.js
@@ -1,11 +1,16 @@
 class Brick {
-    constructor(col, row, color, isBroken) {
+    constructor(col, row, color, isVisual) {
         this.col = col;
         this.row = row;
         this.color = color;
         this.x = this.row * brickSide;
         this.y = this.col * brickSide + 50;
-        this.isBroken = isBroken;
+        this.isVisual = isVisual;
+        this.isBroken = false;
+    }
+
+    setVisual(isVisual){
+        this.isVisual = isVisual;
     }
 
     setBroken(value) {
--- wall.js
+++ wall.js
@@ -1,16 +1,16 @@
-var totalOfColumns = 7;
 var maximumOfColumns = 10;
+var amountOfColumns = 7;
 var totalOfRows = 14;
 var wallOffset = 50;
 
 function wallSetup() {
     for (let row = 0; row < totalOfRows; row++) {
         wall[row] = [];
-        for (let col = 0; col < 10; col++) {
-            if (col < totalOfColumns) {
-                wall[row][col] = new Brick(row, col, randomColor(), false);
+        for (let col = 0; col < maximumOfColumns; col++) {
+            if (col < amountOfColumns) {
+                wall[row][col] = new Brick(row, col, randomColor(), true);
             } else {
-                wall[row][col] = new Brick(row, col, 'black', true);
+                wall[row][col] = new Brick(row, col, 'black', false);
             }
         }
     }
@@ -18,8 +18,8 @@ function wallSetup() {
 
 function wallDraw() {
     for (let row = 0; row < totalOfRows; row++) {
-        for (let col = 0; col < maximumOfColumns; col++) {
-            if (!wall[row][col].isBroken) {
+        for (let col = 0; col < 10; col++) {
+            if (wall[row][col].isVisual) {
                 context.beginPath();
                 context.rect(wall[row][col].x, wall[row][col].y, brickSide, brickSide);
                 context.strokeStyle = 'black';
@@ -36,8 +36,8 @@ function wallDraw() {
 
 function handleBulletAndWall() {
     for (let row = 0; row < totalOfRows; row++) {
-        for (let col = 0; col < totalOfColumns; col++) {
-            if (Physics.collision(wall[row][col], bullet) && !wall[row][col].isBroken) {
+        for (let col = 0; col < maximumOfColumns; col++) {
+            if (Physics.collision(wall[row][col], bullet) && wall[row][col].isVisual) {
                 if (wall[row][col].color == bullet.color) {
                     handleDestroy(row, col);
                 } else if (wall[row][col].y == bullet.y) {
@@ -52,18 +52,17 @@ function handleBulletAndWall() {
 }
 
 function handleDestroy(row, col) {
-    wall[row][col].setBroken(true);
+    wall[row][col].setVisual(false);
     checkAround(row, col, bullet.color);
 }
 
 function handleAppend(row, col) {
     if (col < 9) {
-        wall[row][col + 1] = new Brick(row, col + 1, bullet.color);
+        wall[row][col + 1].setVisual(true);
+        wall[row][col + 1].setColor(bullet.color);
     }
     wallDraw();
 }
-
-
 
 function wallUpdate() {
     checkPullDown();
@@ -80,8 +79,8 @@ function checkAround(row, col, color) {
 
 function checkUp(row, col, color) {
     if (row > 0) {
-        if (!wall[row - 1][col].isBroken && wall[row - 1][col].color == color) {
-            wall[row - 1][col].setBroken(true);
+        if (wall[row - 1][col].isVisual && wall[row - 1][col].color == color) {
+            wall[row - 1][col].setVisual(false);
             checkAround(row - 1, col, color);
         }
     }
@@ -89,8 +88,8 @@ function checkUp(row, col, color) {
 
 function checkDown(row, col, color) {
     if (row < totalOfRows - 1) {
-        if (!wall[row + 1][col].isBroken && wall[row + 1][col].color == color) {
-            wall[row + 1][col].setBroken(true);
+        if (wall[row + 1][col].isVisual && wall[row + 1][col].color == color) {
+            wall[row + 1][col].setVisual(false);
             checkAround(row + 1, col, color);
         }
     }
@@ -98,8 +97,8 @@ function checkDown(row, col, color) {
 
 function checkLeft(row, col, color) {
     if (col > 0) {
-        if (!wall[row][col - 1].isBroken && wall[row][col - 1].color == color) {
-            wall[row][col - 1].setBroken(true);
+        if (!wall[row][col - 1].isVisual && wall[row][col - 1].color == color) {
+            wall[row][col - 1].setVisual(false);
             checkAround(row - 1, col, color);
         }
     }
@@ -107,8 +106,8 @@ function checkLeft(row, col, color) {
 
 function checkRight(row, col, color) {
     if (row < totalOfRows - 1) {
-        if (!wall[row][col + 1].isBroken && wall[row][col + 1].color == color) {
-            wall[row][col + 1].setBroken(true);
+        if (!wall[row][col + 1].isVisual && wall[row][col + 1].color == color) {
+            wall[row][col + 1].setVisual(false);
             checkAround(row, col + 1, color);
         }
     }
@@ -116,8 +115,8 @@ function checkRight(row, col, color) {
 
 function checkPullDown() {
     for (let row = totalOfRows - 1; row > 0; row--) {
-        for (let col = 0; col < totalOfColumns; col++) {
-            if (wall[row][col].isBroken) {
+        for (let col = 0; col < maximumOfColumns; col++) {
+            if (wall[row][col].isVisual) {
                 swapBrick(wall[row][col], wall[row - 1][col]);
             }
         }
@@ -126,9 +125,9 @@ function checkPullDown() {
 
 function checkPullLeft() {
     for (let row = totalOfRows - 1; row > 0; row--) {
-        for (let col = 0; col < totalOfColumns - 1; col++) {
-            if (wall[row][col].isBroken) {
-                swapBrick(wall[row][col], wall[row + 1][col]);
+        for (let col = 0; col < maximumOfColumns - 1; col++) {
+            if (wall[row][col].isVisual == false) {
+                swapBrick(wall[row][col], wall[row][col + 1]);
             }
         }
     }
@@ -136,13 +135,13 @@ function checkPullLeft() {
 
 function swapBrick(firstBrick, secondBrick) {
     let color = firstBrick.color;
-    let isBroken = firstBrick.isBroken;
+    let isVisual = firstBrick.isVisual;
 
     firstBrick.color = secondBrick.color;
-    firstBrick.isBroken = secondBrick.isBroken;
+    firstBrick.isVisual = secondBrick.isVisual;
 
     secondBrick.color = color;
-    secondBrick.isBroken = isBroken;
+    secondBrick.isVisual = isVisual;
 }
 
 function colorEffect(brickColor, bulletColor) {
