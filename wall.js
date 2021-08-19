02cf2387e1b8982c36bc3b2beb88eebb5e2e8166
Nam <62420404+HoangNamDo@users.noreply.github.com>
2021-08-18 17:18:55 -0700

    Update wall.js

--- wall.js
+++ wall.js
@@ -3,36 +3,6 @@ var amountOfColumns = 7;
 var totalOfRows = 14;
 var wallOffset = 50;
 
-function prescribeWall() {
-    for (let row = 0; row < totalOfRows; row++) {
-        wall[row] = [];
-        for (let col = 0; col < maximumOfColumns; col++) {
-            if (col < amountOfColumns) {
-                wall[row][col] = new Brick(row, col, randomColor(), true);
-            } else {
-                wall[row][col] = new Brick(row, col, 'black', false);
-            }
-        }
-    }
-}
-
-function drawWall() {
-    for (let row = 0; row < totalOfRows; row++) {
-        for (let col = 0; col < 10; col++) {
-            if (wall[row][col].isVisual) {
-                context.beginPath();
-                context.rect(wall[row][col].x, wall[row][col].y, brickSide, brickSide);
-                context.strokeStyle = 'black';
-                context.setLineDash([0]);
-                context.stroke();
-                context.fillStyle = wall[row][col].color;
-                context.fill();
-                context.closePath();
-            }
-        }
-    }
-}
-
 function updateWall() {
     shrinkDown();
     shrinkLeft();
@@ -68,7 +38,6 @@ function wallIsCollidedBy(bullet) {
                     appendNewBrick(row, col, bullet);
                 }
                 bullet.disappear();
-                drawWall();
             }
         }
     }
@@ -84,7 +53,6 @@ function appendNewBrick(row, col, bullet) {
         wall[row][col + 1].setVisual(true);
         wall[row][col + 1].setColor(bullet.color);
     }
-    drawWall();
 }
 
 function checkAllAround(row, col, color) {
@@ -130,8 +98,6 @@ function checkRight(row, col, color) {
     }
 }
 
-
-
 function swapBrick(firstBrick, secondBrick) {
     let color = firstBrick.color;
     let isVisual = firstBrick.isVisual;
@@ -141,4 +107,34 @@ function swapBrick(firstBrick, secondBrick) {
 
     secondBrick.color = color;
     secondBrick.isVisual = isVisual;
+}
+
+function prescribeWall() {
+    for (let row = 0; row < totalOfRows; row++) {
+        wall[row] = [];
+        for (let col = 0; col < maximumOfColumns; col++) {
+            if (col < amountOfColumns) {
+                wall[row][col] = new Brick(row, col, randomColor(), true);
+            } else {
+                wall[row][col] = new Brick(row, col, 'black', false);
+            }
+        }
+    }
+}
+
+function drawWall() {
+    for (let row = 0; row < totalOfRows; row++) {
+        for (let col = 0; col < 10; col++) {
+            if (wall[row][col].isVisual) {
+                context.beginPath();
+                context.rect(wall[row][col].x, wall[row][col].y, brickSide, brickSide);
+                context.strokeStyle = 'black';
+                context.setLineDash([0]);
+                context.stroke();
+                context.fillStyle = wall[row][col].color;
+                context.fill();
+                context.closePath();
+            }
+        }
+    }
 }
